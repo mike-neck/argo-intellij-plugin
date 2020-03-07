7beacb441769565f4b1cc019c881730611258c77
Vilmos Nagy <vilmos.nagy@outlook.com>
2020-03-07 18:08:34 +0100

    Workflows are validated against the jsonschema

---
+++ settings.gradle.kts
@@ -0,0 +1,2 @@
+rootProject.name = "argo-intellij-plugin"
+
---
+++ src/main/kotlin/me/vnagy/intellijplugins/argo/providers/ArgoJsonSchemaProviderFactory.kt
@@ -0,0 +1,57 @@
+package me.vnagy.intellijplugins.argo.providers;
+
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.vfs.VfsUtil
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.psi.PsiManager
+import com.jetbrains.jsonSchema.extension.JsonSchemaFileProvider
+import com.jetbrains.jsonSchema.extension.JsonSchemaProviderFactory
+import com.jetbrains.jsonSchema.extension.SchemaType
+import org.jetbrains.yaml.psi.YAMLFile
+import org.jetbrains.yaml.psi.impl.YAMLKeyValueImpl
+
+class ArgoJsonSchemaProviderFactory : JsonSchemaProviderFactory {
+
+    override fun getProviders(project: Project): List<JsonSchemaFileProvider> {
+        return listOf(ArgoJsonSchemaFileProvider(project))
+    }
+}
+
+class ArgoJsonSchemaFileProvider(private val project: Project) : JsonSchemaFileProvider {
+
+    private val psiManager = PsiManager.getInstance(project)
+
+    override fun getName() = "Argo Json Schema"
+
+    override fun isAvailable(file: VirtualFile): Boolean {
+        val psiFile = psiManager.findFile(file)
+        if (psiFile is YAMLFile) {
+            val topLevelValue = psiFile.documents.firstOrNull()?.topLevelValue
+            if (topLevelValue != null) {
+                val apiVersionMatches = topLevelValue
+                    .children
+                    .filterIsInstance(YAMLKeyValueImpl::class.java)
+                    .any { it.keyText == "apiVersion" && it.valueText == "argoproj.io/v1alpha1" }
+                val isWorkflowKind = topLevelValue
+                    .children
+                    .filterIsInstance(YAMLKeyValueImpl::class.java)
+                    .any { it.keyText == "kind" && it.valueText == "Workflow" }
+                return apiVersionMatches && isWorkflowKind
+            }
+        }
+        return false
+    }
+
+    override fun getSchemaFile(): VirtualFile? {
+        return VfsUtil.findFileByURL(
+            ArgoJsonSchemaFileProvider::class
+                .java
+                .getResource("/jsonschema/schemas/workflow.json")
+        )
+    }
+
+    override fun getSchemaType(): SchemaType {
+        return SchemaType.embeddedSchema
+    }
+
+}
