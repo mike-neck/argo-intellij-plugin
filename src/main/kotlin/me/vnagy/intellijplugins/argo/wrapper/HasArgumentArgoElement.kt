f0c1993f10df5449b741888b87662dcf69fb2294
Vilmos Nagy <vilmos.nagy@outlook.com>
2020-05-22 17:05:29 +0200

    #17 Fix the ClassCastException

--- src/main/kotlin/me/vnagy/intellijplugins/argo/wrapper/HasArgumentArgoElement.kt
+++ src/main/kotlin/me/vnagy/intellijplugins/argo/wrapper/HasArgumentArgoElement.kt
@@ -7,7 +7,8 @@ import org.jetbrains.yaml.psi.YAMLValue
 interface HasArgumentArgoElement<T : PsiElement> : ArgoPsi<T> {
 
     val arguments: ArgoArgumentsPsiWrapper?
-        get() = (yamlChildren["arguments"]?.value as YAMLMapping?)?.let { ArgoArgumentsPsiWrapper(it, this) }
+        get() = (yamlChildren["arguments"]?.value as? YAMLMapping?)
+            ?.let { ArgoArgumentsPsiWrapper(it, this) }
 
     val yamlChildren: YAMLValue
 }
