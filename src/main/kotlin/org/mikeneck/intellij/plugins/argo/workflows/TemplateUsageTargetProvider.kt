8734bb6138de627b0169c95a3d51b79be1f7edcf
Shinya.Mochida <shinya.mochida@craftsman-software.com>
2025-08-19 17:35:06 +0900

    refactor parameters for safe operation
    - force psiFile to be YAML File
    - force psiElement to be YAML KeyValue

--- src/main/kotlin/org/mikeneck/intellij/plugins/argo/workflows/TemplateUsageTargetProvider.kt
+++ src/main/kotlin/org/mikeneck/intellij/plugins/argo/workflows/TemplateUsageTargetProvider.kt
@@ -1,40 +1,65 @@
 package org.mikeneck.intellij.plugins.argo.workflows
 
 import com.intellij.openapi.editor.Editor
-import com.intellij.openapi.editor.EditorFactory
-import com.intellij.openapi.fileEditor.FileDocumentManager
-import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
 import com.intellij.usages.UsageTarget
 import com.intellij.usages.UsageTargetProvider
+import me.vnagy.intellijplugins.argo.wrapper.parent
+import org.jetbrains.yaml.YAMLLanguage
+import org.jetbrains.yaml.psi.*
 
-class TemplateUsageTargetProvider: UsageTargetProvider {
+class TemplateUsageTargetProvider : UsageTargetProvider {
 
     override fun getTargets(
         editor: Editor,
         file: PsiFile
     ): Array<out UsageTarget?>? {
+        if (file.language != YAMLLanguage.INSTANCE) {
+            return null
+        }
+        val yamlFile = file as? YAMLFile ?: return null
         val caretModel = editor.caretModel
         val offset = caretModel.offset
-        val element = file.findElementAt(offset) ?: return null
-        return getTargets(editor, file, element)
+        val element = file.findElementAt(offset)?.asArgoWorkflowTemplateNameKeyValue() ?: return null
+        return getTargets(yamlFile, element)
     }
 
     override fun getTargets(psiElement: PsiElement): Array<out UsageTarget?>? {
         val psiFile = psiElement.containingFile ?: return null
-        val virtualFile: VirtualFile = psiFile.viewProvider.virtualFile
-        val document = FileDocumentManager.getInstance().getDocument(virtualFile) ?: return null
-
-        val editorFactory = EditorFactory.getInstance()
-        val project = psiFile.project
-        val editor =
-            editorFactory.getEditors(document, project).firstOrNull() ?: editorFactory.createEditor(document, project)
-            ?: return null
-        return getTargets(editor, psiFile, psiElement)
+        val yamlFile = psiFile as? YAMLFile ?: return null
+        val element = psiElement.asArgoWorkflowTemplateNameKeyValue() ?: return null
+        return getTargets(yamlFile, element)
     }
 
-    fun getTargets(editor: Editor, file: PsiFile, element: PsiElement): Array<out UsageTarget?>? {
+    fun getTargets(file: YAMLFile, element: YAMLKeyValue): Array<out UsageTarget?>? {
         TODO("Not yet implemented")
     }
+}
+
+val PsiElement.isYamlKey: Boolean
+    get() {
+        if (this is YAMLKeyValue) {
+            return true
+        }
+        return this.parent is YAMLKeyValue
+    }
+
+fun PsiElement.asArgoWorkflowTemplateNameKeyValue(): YAMLKeyValue? {
+    if (this !is YAMLScalar && !(this.isYamlKey) && this !is YAMLKeyValue) {
+        return null
+    }
+    var element = this
+    while (element !is YAMLKeyValue) {
+        element = element.parent ?: return null
+    }
+    if (element.keyText != "name") {
+        return null
+    }
+    val templates = (element.parent<YAMLMapping>()
+        .parent<YAMLSequenceItem>()
+        .parent<YAMLSequence>()
+        .parent<YAMLKeyValue>()
+        ?: return null)
+    return if (templates.keyText == "templates") element else null
 }
